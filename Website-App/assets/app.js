dfae8f028d2b7be73fd042b36dba90442d7430b7
Manny Cespedes <mgcesp@gmail.com>
2016-01-18 21:51:37 -0500

    updated Website-App with Angular routing

--- Website-App/assets/app.js
+++ Website-App/assets/app.js
@@ -1,3 +1,64 @@
-var app = angular.module("computer",['ngRoute'])
+var app = angular.module('computer',['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider)]);
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/main', {
+		templateUrl: 'main.html',
+		controller: 'MainController'
+	}).
+	when('/about', {
+		templateUrl: 'about.html',
+		controller: 'MainController'
+	}).
+	when('/services', {
+		templateUrl: 'services.html',
+		controller: 'ServicesController'
+	}).
+	when('/contact', {
+		templateUrl: 'contact.html',
+		controller: 'ContactController'
+	}).
+	otherwise({redirectTo:'/main'})
+}])
+
+.controller('MainController', ['$scope', '$http', function($scope, $http){
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+		console.log(response.data);
+	});
+}])
+
+.controller('ServicesController', ['$scope', '$http', function($scope, $http){
+	$http.get('services.json').then(function(response){
+		$scope.services = response.data;
+		console.log(response.data);
+	});
+}])
+
+.controller('ContactController', ['$scope', '$http', function($scope, $http){
+	$http.get('locations.json').then(function(response){
+		$scope.locations = response.data;
+		console.log(response.data);
+	});
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
